79532d6149160fe8fe41ed5b0a0e3498476cf080
andre.luiz <andre.lui@fabrica.locaweb.com.br>
2024-06-13 16:19:56 -0300

    feat: initial commit

---
+++ app/javascript/components/Abc.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <div>
+    <h2>Eu sou uma batata {{ color }}</h2>
+
+    <button @click="increment">
+      Increment
+    </button>
+
+    <span>
+      {{  count  }}
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        color: 'purple',
+        count: 0
+      }
+    },
+    methods: {
+      increment () {
+        this.count = this.count + 1
+      }
+    }
+  }
+</script>
---
+++ app/javascript/components/App.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <div>
+    <h1>Hello World!</h1>
+    <Abc />
+  </div>
+</template>
+
+<script>
+  import Abc from './Abc.vue';
+  export default {
+    components: {
+      Abc
+    }
+  }
+</script>
+
+<style lang="">
+
+</style>
